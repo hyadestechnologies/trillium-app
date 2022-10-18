6c1c0375b624c9eb96f5f171e017922bd8aa88bf
Cristian Scapin <cristian.scapin654@icloud.com>
2022-10-18 10:34:52 +0200

    all files formatted

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TestComponent from "@/components/TestComponent.vue";
+import TestComponent from '@/components/TestComponent.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: TestComponent,
     },
     {
