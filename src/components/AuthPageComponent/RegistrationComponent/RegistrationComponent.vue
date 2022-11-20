efbddff5ed9595cb8e27b5f4933e0c60d28a0d1c
Matteo Brusarosco <matteo.brusarosc@gmail.com>
2022-11-20 12:10:28 +0100

    feat: add account finalization form

--- src/components/AuthPageComponent/RegistrationComponent/RegistrationComponent.vue
+++ src/components/AuthPageComponent/RegistrationComponent/RegistrationComponent.vue
@@ -5,32 +5,47 @@ export default defineComponent({
     return {
       user: {
         firstName: '',
-        lastName: '',
+        surname: '',
         username: '',
       },
       formErrors: {
         firstName: false,
-        lastName: false,
+        surname: false,
         username: false,
       },
+      isFormInit: true,
     };
   },
   methods: {
-    onFormSubmit() {
+    onFormSubmitInit() {
       // do checks
       if (!this.user.firstName) {
-        console.log('first name is not set');
+        this.formErrors.firstName = true;
       }
 
-      if (!this.user.lastName) {
-        // do something
+      if (!this.user.surname) {
+        this.formErrors.surname = true;
       }
 
       if (!this.user.username) {
-        // do something
+        this.formErrors.surname = true;
       }
 
-      console.log('form has been submitted');
+      if (this.user.firstName && this.user.surname && this.user.username) {
+        console.log('form is valid');
+        this.isFormInit = false;
+      }
+    },
+    onFormFinalize() {
+      // do checks
+    },
+    onFormBack() {
+      this.isFormInit = true;
+    },
+  },
+  computed: {
+    formHeader() {
+      return this.isFormInit ? 'Tell me who you are!' : 'Finalize creation';
     },
   },
 });
@@ -38,14 +53,23 @@ export default defineComponent({
 
 <template>
   <div class="flex flex-col mt-5 p-5">
-    <div class="form-header">
-      <h1 class="text-center text-2xl">Tell me who you are!</h1>
+    <div class="form-header mb-5">
+      <h1 class="text-center text-2xl">{{ formHeader }}</h1>
     </div>
-    <form class="form-body flex flex-col gap-10" @submit.prevent="onFormSubmit">
+    <form v-if="isFormInit" class="form-body-init flex flex-col gap-10" @submit.prevent="onFormSubmitInit">
       <input type="text" v-model="user.firstName" placeholder="First name" class="rounded-lg indent-2" />
-      <input type="text" v-model="user.lastName" placeholder="Last name" class="rounded-lg indent-2" />
+      <input type="text" v-model="user.surname" placeholder="Last name" class="rounded-lg indent-2" />
       <input type="text" v-model="user.username" placeholder="Username" class="rounded-lg indent-2" />
       <button class="btn-default">Continue</button>
+    </form>
+    <form v-else class="form-body-fin w-full flex flex-col gap-10" @submit.prevent="onFormFinalize">
+      <input type="text" placeholder="Email" class="rounded-lg indent-2" />
+      <input type="text" placeholder="Password" class="rounded-lg indent-2" />
+      <input type="text" placeholder="Repeat password" class="rounded-lg indent-2" />
+      <div class="btn-container flex flex-row gap-5">
+        <button type="button" @click="onFormBack" class="btn-default">Back</button>
+        <button type="submit" class="btn-default">Register</button>
+      </div>
     </form>
   </div>
 </template>
