7c8bea6f8d3c2f880a17ddc091a427bf36f9dfd8
Matteo Brusarosco <matteo.brusarosc@gmail.com>
2022-11-23 08:04:11 +0100

    install axios and setup

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
-
+import axios from './plugins/axios';
 import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(axios, {
+  baseUrl: 'http://192.168.1.250:8000',
+});
 
 app.mount('#app');
