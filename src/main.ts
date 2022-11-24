fb6b22f4163570d6ef506308397ccb17f4435e43
MattBrs <matteo.brusarosc@gmail.com>
2022-11-24 09:25:02 +0100

    Add use query and add userSignup mutation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import { VUE_QUERY_CLIENT, QueryClient, VueQueryPlugin } from '@tanstack/vue-query';
 
 import './assets/main.css';
 
@@ -13,9 +14,17 @@ const axiosInstance = axios.create({
   baseURL: 'https://trilliumdev-staging.up.railway.app/',
 });
 
+export const vueQueryPlugin = {
+  install: (app: any, options: any) => {
+    const queryClient = new QueryClient(options);
+    app.provide(VUE_QUERY_CLIENT, queryClient);
+  },
+};
+
 app.use(createPinia());
 app.use(router);
 
 app.use(VueAxios, axiosInstance);
 app.provide('axios', app.config.globalProperties.axios);
+app.use(VueQueryPlugin);
 app.mount('#app');
