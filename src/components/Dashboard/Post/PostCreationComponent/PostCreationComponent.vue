e45d844666b7776d19ee2998e348b977ca2c2a3f
MattBrs <matteo.brusarosc@gmail.com>
2022-12-18 22:53:26 +0100

    fix some styles

--- src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
+++ src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
@@ -47,11 +47,16 @@ function createPost() {
         <h1 class="font-bold">Create new post</h1>
         <div class="form-container flex flex-col gap-1">
             <h3 class="mt-5 font-bold">Post title</h3>
-            <input type="text" placeholder="Title" v-model="post.title"> 
+            <input class="rounded-xl indent-2" type="text" placeholder="Title" v-model="post.title"> 
             <h3 class="mt-5 font-bold">Post description</h3>
-            <textarea name="description" id="descriptionTextArea" cols="30" rows="2" placeholder="Post description" v-model="post.description"></textarea>
+            <textarea class="rounded-xl indent-2 resize-none" maxlength="255" name="description" id="descriptionTextArea" cols="30" rows="4" placeholder="Post description" v-model="post.description"></textarea>
             <h3 class="mt-5 font-bold" v>Post media (photo or video)</h3>
-            <input type="file" @change="uploadFile">
+            <input class="block w-full text-sm text-black
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-full file:border-0
+                file:text-sm file:font-semibold
+                file:bg-orange-400 file:text-black
+                hover:file:bg-red-100" type="file" @change="uploadFile">
             <button class="mt-10 btn-default" @click="createPost">Create post</button>
         </div>
     </div>
