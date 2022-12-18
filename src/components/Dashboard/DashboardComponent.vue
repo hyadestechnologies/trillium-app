c3a2c0348007d5a4c8ed26f0a3b2f1bb82d7455e
MattBrs <matteo.brusarosc@gmail.com>
2022-12-18 22:36:34 +0100

    add create post form and initial functions

--- src/components/Dashboard/DashboardComponent.vue
+++ src/components/Dashboard/DashboardComponent.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-    <div class="h-full w-full flex flex-row gap-10 items-center justify-center bg-red-300">
-        <div class="posts h-full w-full">
+    <div class="h-full w-full flex flex-col md:flex-row gap-10 items-center justify-center bg-red-300">
+        <div class="posts h-full w-4/5 md:w-2/3 overflow-y-auto">
             <router-view />
         </div>
-        <div class="dashboard-functions h-full w-full">this container contains dashboard controls</div>
+        <div class="dashboard-functions h-full w-4/5 md:w-1/3 bg-red-400">this container contains dashboard controls (like search, create post btn, ecc)</div>
     </div>
 </template>
--- src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
+++ src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
@@ -1,13 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMutation, useQueryClient } from '@tanstack/vue-query';
+import { inject, reactive } from 'vue';
+import type { Post } from './PostCreationComponentTypes';
+
+const axios: any = inject('axios');
+const queryClient = useQueryClient();
+
+
+
+const post:Post = reactive({
+    title: "",
+    description: "",
+});
+
+
+const createPostMutation = useMutation({
+  mutationFn: (newPost: Post) => {
+    return axios
+      .post('/post/create', {
+        title: newPost.title,
+        description: newPost.description,
+        file: newPost.file,  
+      })
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  },
+});
+
+function uploadFile(event: any) {
+    post.file = event.target.files[0];
+}
+
+function createPost() {
+    if (post.title && post.description) {
+        createPostMutation.mutate(post);
+    }
+}
+
+</script>
 <template>
-    <div class="h-100 flex flex-col">
-        <h1>Create new post</h1>
+    <div class="h-100 flex flex-col p-5">
+        <h1 class="font-bold">Create new post</h1>
         <div class="form-container flex flex-col gap-1">
-            <input type="text" placeholder="Title"> 
-            <textarea name="description" id="descriptionTextArea" cols="30" rows="2" placeholder="Post description"></textarea>
-            <h3 class="mt-5 font-bold">Upload a file or video you want to share</h3>
-            <input type="file" >
-            <button class="mt-10">Upload</button>
+            <h3 class="mt-5 font-bold">Post title</h3>
+            <input type="text" placeholder="Title" v-model="post.title"> 
+            <h3 class="mt-5 font-bold">Post description</h3>
+            <textarea name="description" id="descriptionTextArea" cols="30" rows="2" placeholder="Post description" v-model="post.description"></textarea>
+            <h3 class="mt-5 font-bold" v>Post media (photo or video)</h3>
+            <input type="file" @change="uploadFile">
+            <button class="mt-10 btn-default" @click="createPost">Create post</button>
         </div>
     </div>
 </template>
