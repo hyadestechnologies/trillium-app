86ddc363753d8855c6cb1f8654e0760eff368cf3
MattBrs <matteo.brusarosc@gmail.com>
2022-12-18 21:32:22 +0100

    add basic dashboard structure to start implementing post creation page

---
+++ src/components/Dashboard/DashboardComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="container h-100 flex flex-row gap-10 items-center justify-center">
+        <div class="posts w-2/3 h-100">
+            <router-view />
+        </div>
+        <div class="dashboard-functions w-1/3 h-100">this container contains dashboard controls</div>
+    </div>
+</template>
---
+++ src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+    <h1>this is the post creation page</h1>
+</template>
