cc509c90a6f5a5a1656bef26ebf132420264c6b8
MattBrs <matteo.brusarosc@gmail.com>
2022-12-18 21:53:53 +0100

    add initial post creation pge setup

--- src/components/Dashboard/DashboardComponent.vue
+++ src/components/Dashboard/DashboardComponent.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 <template>
-    <div class="container h-100 flex flex-row gap-10 items-center justify-center">
-        <div class="posts w-2/3 h-100">
+    <div class="h-full w-full flex flex-row gap-10 items-center justify-center bg-red-300">
+        <div class="posts h-full w-full">
             <router-view />
         </div>
-        <div class="dashboard-functions w-1/3 h-100">this container contains dashboard controls</div>
+        <div class="dashboard-functions h-full w-full">this container contains dashboard controls</div>
     </div>
 </template>
--- src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
+++ src/components/Dashboard/Post/PostCreationComponent/PostCreationComponent.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts"></script>
 <template>
-    <h1>this is the post creation page</h1>
+    <div class="h-100 flex flex-col">
+        <h1>Create new post</h1>
+        <div class="form-container flex flex-col gap-1">
+            <input type="text" placeholder="Title"> 
+            <textarea name="description" id="descriptionTextArea" cols="30" rows="2" placeholder="Post description"></textarea>
+            <h3 class="mt-5 font-bold">Upload a file or video you want to share</h3>
+            <input type="file" >
+            <button class="mt-10">Upload</button>
+        </div>
+    </div>
 </template>
