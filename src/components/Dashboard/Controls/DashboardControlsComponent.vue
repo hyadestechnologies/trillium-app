94d58c5e07df8a6dcce1a7abca948cc1a186167c
Antonio Gelain <antonio.gelain2@gmail.com>
2023-05-07 17:22:13 +0200

    feat: initial tags filter component design

--- src/components/Dashboard/Controls/DashboardControlsComponent.vue
+++ src/components/Dashboard/Controls/DashboardControlsComponent.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import SearchBarComponent from './SearchBar/SearchBarComponent.vue';
+import TagFilterListComponent from '../Post/TagFilterComponent/TagFilterListComponent.vue';
+
+// TODO: Load tags
 </script>
 <template>
-  <div class="">
-    <div class="mt-10 btn-default items-center justify-center">
-      <RouterLink to="/dashboard/createpost" class="flex flex-col items-center justify-center">Create post</RouterLink>
-    </div>
+  <div class="my-2 btn-default items-center justify-center">
+    <RouterLink to="/dashboard/createpost" class="flex flex-col items-center justify-center">Create post</RouterLink>
   </div>
+  <TagFilterListComponent :tags="[]" />
 </template>
---
+++ src/components/Dashboard/Post/TagFilterComponent/TagFilterListComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TagFilterComponent from './TagFilterComponent.vue';
+
+defineProps<{
+  tags: string[];
+}>();
+</script>
+<template>
+  <div class="flex flex-col justify-center mt-4">
+    <h3 class="border-b-2 border-black">Tags</h3>
+
+    <ul v-for="tag in tags">
+      <li><TagFilterComponent :tag="tag" /></li>
+    </ul>
+  </div>
+</template>
